35d8c6fdb669f43f808bf19dd570d51c3d6b9439
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-03-23 20:56:52 +0530

    Delete bms directory

--- bms/src/main/java/bms/project/demo/repository/NeftTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package bms.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import bms.project.demo.Entity.NeftTransaction; 
-
-public interface NeftTransactionRepository extends JpaRepository<NeftTransaction,Integer>{
-
-}
--- bms/src/main/java/bms/project/demo/repository/RtgsTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package bms.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import bms.project.demo.Entity.RtgsTransaction; 
-
-public interface RtgsTransactionRepository extends JpaRepository<RtgsTransaction,Integer>{
-
-}
