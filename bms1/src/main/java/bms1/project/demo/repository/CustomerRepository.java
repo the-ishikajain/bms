ca51450cfad1076667e355d44cd39e1149a6cc3b
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2023-03-28 21:07:18 +0530

    Delete bms1 directory

--- bms1/src/main/java/bms1/project/demo/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package bms1.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import bms1.project.demo.Entity.Customer; 
-
-public interface CustomerRepository extends JpaRepository<Customer,String>{
-
-}
--- bms1/src/main/java/bms1/project/demo/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package bms1.project.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import bms1.project.demo.Entity.Employee; 
-
-public interface EmployeeRepository extends JpaRepository<Employee,String>{
-
-}
